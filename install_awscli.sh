2b138ab5f3a6f623da8f856f265e478b592b8b4f
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-05-22 14:48:01 -0300

    Create install_awscli.sh

---
+++ install_awscli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt-get install curl -y
+sudo apt-get install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
