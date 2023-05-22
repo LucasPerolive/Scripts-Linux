804fcd0024b803c5d7f4cd5d3a952fc028faf5d4
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-05-22 14:44:18 -0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -2,13 +2,17 @@
 sudo apt update -y
 
 sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common -y
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt update -y
-
 apt-cache policy docker-ce
 sudo apt install docker-ce -y
-sudo systemctl status docker
 sudo snap install docker
+
+sudo systemctl status docker >> /dev/null
+if [ $? -eq 0 ]
+then
+  echo "Docker incializado!"
+else
+  echo "Nao foi possivel inicializar o docker!"
+fi
