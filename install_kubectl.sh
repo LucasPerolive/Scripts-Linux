6ebfbc6c1249e22059452238453052b06ab4ac0c
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-05-24 11:03:23 -0300

    Update install_kubectl.sh

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -4,3 +4,10 @@ sudo apt-get install curl -y
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+if [ $? -eq 0 ]
+then
+  echo "O Kubectl foi instalado com sucesso!"
+else
+  echo "Nao foi possivel instalar o Kubectl!"
+fi
