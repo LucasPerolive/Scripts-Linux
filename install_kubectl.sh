89ac3535438542170e1517c542362417dfd2b06c
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-05-24 15:17:58 -0300

    Update install_kubectl.sh

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
-sudo apt-get install curl -y
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/
+# Instalação do kubectl
+echo "Instalando o kubectl..."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 
-if [ $? -eq 0 ]
-then
-  echo "O Kubectl foi instalado com sucesso!"
-else
-  echo "Nao foi possivel instalar o Kubectl!"
-fi
+# Instalação do Docker
+echo "Instalando o Docker..."
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+
+# Instalação do Minikube
+echo "Instalando o Minikube..."
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Configuração do ambiente do Minikube
+echo "Configurando o ambiente do Minikube..."
+minikube config set driver docker
+
+# Inicialização do Minikube
+echo "Iniciando o Minikube..."
+minikube start
+
+# Verificação das versões instaladas
+echo "Versões instaladas:"
+kubectl version --client
+docker version
+minikube version
