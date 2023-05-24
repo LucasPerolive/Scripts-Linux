6ca584c61ef510a44fae8c70d2035b7ee5db5059
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-05-24 10:24:43 -0300

    Update install_minikube.sh

--- install_minikube.sh
+++ install_minikube.sh
@@ -2,3 +2,10 @@
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.12.1/minikube-linux-amd64 \ && chmod +x minikube
 sudo install minikube /usr/local/bin/
+
+if [ $? -eq 0 ]
+then
+  echo "O minikube foi instalado com sucesso!"
+else
+  echo "Não foi possivel instalar o minikube!"
+fi
