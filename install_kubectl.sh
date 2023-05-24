e71216f55d08223e25f13b75687f59ba3bc8a556
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-05-24 14:36:26 -0300

    Update install_kubectl.sh

--- install_kubectl.sh
+++ install_kubectl.sh
@@ -3,7 +3,7 @@
 sudo apt-get install curl -y
 curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+sudo mv ./kubectl /usr/local/bin/
 
 if [ $? -eq 0 ]
 then
